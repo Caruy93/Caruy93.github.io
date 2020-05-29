865ba1f72b32dc2200f41fe6f528a8c7a473eaa5
Caruy <caruychen@gmail.com>
2020-05-29 14:03:05 +0300

    begin implementing open weather api

---
+++ resources/javascript/openweather.js
@@ -0,0 +1,20 @@
+// Script uses OpenWeather API to provide Helsinki's weather information
+const openWeatherKey = '1eb909d1a2afc9bf25c25ffd2b76fffe';
+const openWeatherUrl = 'api.openweathermap.org/data/2.5/weather';
+
+// Fetch weather information
+const getForecast = async() => {
+    const urlToFetch = `${openWeatherUrl}?q=helsinki&appid={openWeatherKey}`;
+
+    try {
+
+        const response = await fetch(urlToFetch);
+
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+            return jsonResponse;
+        }
+
+    } catch(error) {console.log(error);}
+}
