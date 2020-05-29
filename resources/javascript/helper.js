39abb67da256bd79a93139decf3121969785edaa
Caruy <caruychen@gmail.com>
2020-05-29 15:00:07 +0300

    weather api implemented into html text

--- resources/javascript/helper.js
+++ resources/javascript/helper.js
@@ -1,5 +1,47 @@
 // Convert open weather json information for html rendering
 const createWeatherHTML = (currentDay) => {
-    console.log(currentDay);
-    return `<p>${currentDay.weather[0].main}</p>`
+
+    let tempDescription;
+    let mainCondition;
+
+    if (currentDay.main.temp < 18) {
+        tempDescription = 'cold';
+    } else {
+        tempDescription = 'warm';
+    }
+
+    switch(currentDay.weather[0].description) {
+        case 'clear sky':
+            mainCondition = 'sunny';
+            break;
+        case 'few clouds':
+            mainCondition = 'cloudy';
+            break;
+        case 'scattered clouds':
+            mainCondition = 'cloudy';
+            break;
+        case 'broken clouds':
+            mainCondition = 'cloudy';
+            break;
+        case 'shower rain':
+            mainCondition = 'rainy';
+            break;
+        case 'rain':
+            mainCondition = 'rainy';
+            break;
+        case 'thunderstorm':
+            mainCondition = 'stormy';
+            break;
+        case 'snow':
+            mainCondition = 'snowy';
+            break;
+        case 'mist':
+            mainCondition = 'misty';
+            break;
+        default:
+            mainCondition = 'unknown';
+    }
+
+    return tempDescription + ' and ' +  mainCondition;
+    
 }
--- resources/javascript/openweather.js
+++ resources/javascript/openweather.js
@@ -1,10 +1,13 @@
 // Script uses OpenWeather API to provide Helsinki's weather information
 const openWeatherKey = '1eb909d1a2afc9bf25c25ffd2b76fffe';
-const openWeatherUrl = 'api.openweathermap.org/data/2.5/weather';
+const openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Access inline weather text element
+const weatherElement = document.getElementById('weather-update');
 
 // Fetch weather information
 const getForecast = async() => {
-    const urlToFetch = `${openWeatherUrl}?q=helsinki&appid={openWeatherKey}`;
+    const urlToFetch = `${openWeatherUrl}?q=helsinki&appid=${openWeatherKey}`;
 
     try {
 
@@ -12,7 +15,7 @@ const getForecast = async() => {
 
         if (response.ok) {
             const jsonResponse = await response.json();
-            console.log(jsonResponse);
+
             return jsonResponse;
         }
 
@@ -22,12 +25,16 @@ const getForecast = async() => {
 // Render forecast text
 const renderForecast = (day) => {
     let weatherContent = createWeatherHTML(day)
+    return weatherContent;
 }
 
 // Execute weather update
-const executeUpdate = () => {
-    getForecast().then(forecast => renderForecast(forecast))
+const executeUpdate = async() => {
+    const weatherUpdate = await getForecast().then(forecast => renderForecast(forecast));
+
+    weatherElement.innerHTML = `${weatherUpdate}`;
     return false;
 }
 
 executeUpdate();
+
