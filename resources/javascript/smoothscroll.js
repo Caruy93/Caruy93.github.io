f866ee93d8192b58d3bfd92360c0f407e08c6db3
Caruy <caruychen@gmail.com>
2020-06-25 16:45:37 +0300

    Implement smooth scrolling to jQuery

--- resources/javascript/smoothscroll.js
+++ resources/javascript/smoothscroll.js
@@ -1,14 +1,26 @@
-const mainNavLinks = document.querySelectorAll("nav ol li a");
+$(document).ready(function() {
 
-mainNavLinks.forEach(link => {
-    link.addEventListener("click", event => {
-        
-        event.preventDefault();
-        let target = document.querySelector(event.target.hash);
-        console.log(target);
-        target.scrollIntoView({
-            behavior: "smooth",
-            block: "start"
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            let hash = this.hash;
+
+            // Use Jquery'y animate() method to add smooth page scroll
+            // The optional number (800) specifies number of ml seconds it takes to scroll to the specified area
+            $('html, body').animate(
+                {scrollTop: $(hash).offset().top},
+                800,
+                function() {
+                    //Add hash (#) to URL when done scrolling (Default click behavior)
+                    window.location.hash = hash;
+            });
+        } // End of if
     });
-  });
 });
